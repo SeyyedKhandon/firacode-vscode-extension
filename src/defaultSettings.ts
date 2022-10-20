b66c1d34c22905a68077f2cc4b597872fa0e79b8
SeyyedKhandon <seyyedkhandon@gmail.com>
2022-10-20 12:50:09 +0330

    chore: :recycle: simplify update config process

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -1,6 +1,7 @@
-import { GeneralObject } from "./util";
-
-export const defaultSettings = [
-  { "editor.fontFamily": "Fira Code" },
-  { "editor.fontLigatures": true },
-] as GeneralObject[];
+export interface GeneralObject {
+  [index: string]: string | number | boolean | GeneralObject;
+}
+export const defaultSettings = {
+  "editor.fontFamily": "Fira Code",
+  "editor.fontLigatures": true
+};
